5a6b1b2ba60c130c5a06022e3ac58252836d0269
leo zacharry <leo.zacharry@gmail.com>
2018-08-16 23:49:48 +0300

    Express server
    EXPRESS HTTP DATABASE

---
+++ Remote Database (Express Server)/mira_express_database.js
@@ -0,0 +1,45 @@
+    var bodyParser = require("body-parser");
+    var express = require('express');
+    var mira = require('mira-db');
+    var UserData = require('mira-db/userdata');
+    var app = express();
+ 
+    var USR_PERM = { SELECT:true,ADD:true,UPDATE:true,RENAME:true,DROP:true,DELETE:true,CREATE:true,LIST:true } ;
+    var DB_DIR = "./data";
+
+    app.use(express.static('public')); 
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(function (req, res, next) {  next(); });
+    app.get('/', function (req, res, next){
+        res.sendFile( process.cwd()  +'/public/index.html');
+    });
+
+     app.post('/query', function (req, res, next){
+        var DB_NAME = req.body["db"];
+        var DB_USER = req.body["user"];
+        var DB_PASS = req.body["pass"];
+        var DB_QUERY = req.body["query"];
+        var expressDB = new mira(DB_DIR,"UTF-8");
+
+    var LOGIN_DATA = UserData.LoadLoginData("user.conf"); 
+            if(LOGIN_DATA[0]){
+                var result = UserData.Login( DB_USER , DB_PASS , LOGIN_DATA ) ;
+                var USR_PERM = UserData.LoadPermsData(result["perm"]+".perm");
+                if (result){ 
+                        if(result["db"]=="*"){
+                            var DB_RESULT = expressDB.Query(DB_QUERY,DB_NAME,USR_PERM);
+                            res.send(DB_RESULT);
+                        } else {
+                            var DB_RESULT = expressDB.Query(DB_QUERY,result["db"],USR_PERM);
+                            res.send(DB_RESULT);
+                        }
+                }else{
+                    res.status(500).send([false,"LOGIN_ERROR","INVALID USERNAME OR PASSWORD",0]);
+                }   
+                } else {
+                    res.status(500).send([false,"SERVER_ERROR","INVALID CONF FILE",100]);
+            }
+    });
+
+    var server = app.listen(8123);
--- Remote Database (Express Server)/readme.txt
+++ Remote Database (Express Server)/readme.txt
@@ -1,1 +1,10 @@
-add coming soon example server "please follow github"
+	MIRA DB EXPRESS DATABASE (HTTP BASED)
+	
+	## INSTALL MIRA ##
+	npm install mira-db 
+
+	## INSTALL EXPRESS ##
+	npm install express 
+
+	## INSTALL BODY PARSER ##
+	npm install body-parser 
