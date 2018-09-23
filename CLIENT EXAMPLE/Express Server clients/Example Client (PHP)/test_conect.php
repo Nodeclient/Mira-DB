012b75b68a52967a53ecf1d8b986c892caa74221
leo zacharry <leo.zacharry@gmail.com>
2018-09-23 04:15:28 +0300

    Mira Database Server
    readme

--- CLIENT EXAMPLE/Express Server clients/Example Client (PHP)/test_conect.php
+++ CLIENT EXAMPLE/Express Server clients/Example Client (PHP)/test_conect.php
@@ -1,5 +1,5 @@
 <?php
-// MiraDB ClASS EXAMPLE
+// MiraDB CLASS EXAMPLE
 // [GITHUB]/Mira-DB/CONNECTOR/PHP/miradb_php_connector.php 
     include 'miradb_php_connector.php';
         $db = new MiraDB("root","","http://localhost:8123/query","test");
