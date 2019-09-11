d3685a1922b9387bd16331650846673c8dd1083b
Nodeclient <41586831+Nodeclient@users.noreply.github.com>
2019-09-11 04:02:26 +0300

    Mira-DB 1.0.39
    set new query folder

--- sources/mira-dev-1.0.39/lib/query/register.js
+++ sources/mira-dev-1.0.39/lib/query/register.js
@@ -3,32 +3,32 @@ module.exports.TAG = require('./engine_string/mira_tag')(); //+
 module.exports.STRING = require('./engine_string/mira_string')(); //+
 module.exports.JS = require('./engine_string/mira_parse')(); //+
 /*UPDATE-UNIQUE*/
-module.exports.UNIQUE_COL = require('./unique_column')(); //+
-module.exports.UPDATE_ROW = require('./update_row')(); //+
+module.exports.UNIQUE_COL = require('./database_string/unique_column')(); //+
+module.exports.UPDATE_ROW = require('./database_string/update_row')(); //+
 /*SELECT*/
-module.exports.SELECT_TABLE = require('./select_table')(); //+
-module.exports.SELECT_COLUMN = require('./select_column')(); //+
-module.exports.SELECT_LIMIT = require('./select_limit')(); //+
-module.exports.SELECT_FIND = require('./select_find')(); //+
-module.exports.SELECT_FIND_LIKE = require('./select_find_like')(); //+
-module.exports.SELECT_COUNT = require('./select_table_count')(); //+
+module.exports.SELECT_TABLE = require('./database_string/select_table')(); //+
+module.exports.SELECT_COLUMN = require('./database_string/select_column')(); //+
+module.exports.SELECT_LIMIT = require('./database_string/select_limit')(); //+
+module.exports.SELECT_FIND = require('./database_string/select_find')(); //+
+module.exports.SELECT_FIND_LIKE = require('./database_string/select_find_like')(); //+
+module.exports.SELECT_COUNT = require('./database_string/select_table_count')(); //+
 /*ADD*/
-module.exports.ADD_ROW = require('./add_row')(); //+
-module.exports.ADD_COLUMN = require('./add_column')(); //+
+module.exports.ADD_ROW = require('./database_string/add_row')(); //+
+module.exports.ADD_COLUMN = require('./database_string/add_column')(); //+
 /*RENAME*/
-module.exports.RENAME_TABLE = require('./rename_table')(); //+
-module.exports.RENAME_COLUMN = require('./rename_column')(); //+
-module.exports.RENAME_DATABASE = require('./rename_database')(); //+
+module.exports.RENAME_TABLE = require('./database_string/rename_table')(); //+
+module.exports.RENAME_COLUMN = require('./database_string/rename_column')(); //+
+module.exports.RENAME_DATABASE = require('./database_string/rename_database')(); //+
 /*DROP*/
-module.exports.DROP_TABLE = require('./drop_table')(); //+
-module.exports.DROP_DATABASE = require('./drop_database')(); //+
+module.exports.DROP_TABLE = require('./database_string/drop_table')(); //+
+module.exports.DROP_DATABASE = require('./database_string/drop_database')(); //+
 /*DELETE*/
-module.exports.DELETE_COLUMN = require('./delete_column')(); //+
-module.exports.DELETE_ROW = require('./delete_row')(); //+
-module.exports.DELETE_INDEX = require('./delete_index_row')(); //+
+module.exports.DELETE_COLUMN = require('./database_string/delete_column')(); //+
+module.exports.DELETE_ROW = require('./database_string/delete_row')(); //+
+module.exports.DELETE_INDEX = require('./database_string/delete_index_row')(); //+
 /*CREATE*/
-module.exports.CREATE_TABLE = require('./create_table')(); //+
-module.exports.CREATE_DATABASE = require('./create_database')(); //+
+module.exports.CREATE_TABLE = require('./database_string/create_table')(); //+
+module.exports.CREATE_DATABASE = require('./database_string/create_database')(); //+
 /*LIST*/
-module.exports.LIST_DATABASE = require('./list_database')(); //+
-module.exports.LIST_TABLE = require('./list_table')(); //+
+module.exports.LIST_DATABASE = require('./database_string/list_database')(); //+
+module.exports.LIST_TABLE = require('./database_string/list_table')(); //+
